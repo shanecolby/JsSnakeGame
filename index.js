fe3ba0f860adbc3ce4a61407e65c74d0fec5d654
shanecolby <shanecolbyzero8@gmail.com>
2021-08-10 17:19:05 -0500

    remove tail and add index to make movement of snake and add styling

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const start = document.getElementById("start")
 const score = document.getElementById("score")
 let squares = []
 let currentSnake = [2, 1, 0]
+let direction = 1
 
 function createGrid() {
   for (let i = 0; i < 100; i++) {
     const square = document.createElement('div')
-    console.log(square)
     square.classList.add('square')
     grid.appendChild(square)
     squares.push(square)
   }
-  console.log(squares)
+
 }
 createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake'))
+
+function move() {
+  const tail = currentSnake.pop()
+  squares[tail].classList.remove("snake")
+  currentSnake.unshift(currentSnake[0] + direction)
+  squares[currentSnake[0]].classList.add("snake")
+}
+move()
