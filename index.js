da4de110404893bb364e8024f5035f0da133113f
shanecolby <shanecolbyzero8@gmail.com>
2021-08-10 17:01:14 -0500

    style snake and put currentSnake into squares array to display the snake on the grid

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector(".grid")
 const start = document.getElementById("start")
 const score = document.getElementById("score")
-
+let squares = []
+let currentSnake = [2, 1, 0]
 
 function createGrid() {
   for (let i = 0; i < 100; i++) {
@@ -9,6 +10,10 @@ function createGrid() {
     console.log(square)
     square.classList.add('square')
     grid.appendChild(square)
+    squares.push(square)
   }
+  console.log(squares)
 }
 createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
