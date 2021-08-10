7c006565b8995f448a0eff4af04f25dcb1a37783
shanecolby <shanecolbyzero8@gmail.com>
2021-08-10 16:55:00 -0500

    style square, create grid, append div squares within grid

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const grid = document.querySelector(".grid")
 const start = document.getElementById("start")
 const score = document.getElementById("score")
+
+
+function createGrid() {
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement('div')
+    console.log(square)
+    square.classList.add('square')
+    grid.appendChild(square)
+  }
+}
+createGrid()
