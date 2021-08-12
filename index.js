1a06fcabcdb40c83531ac5d3d05a0412827494a1
shanecolby <shanecolbyzero8@gmail.com>
2021-08-12 15:40:37 -0500

    add conditional to move function that will clear timerId interval if snake hits the wall or travels back on itself

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+  if (
+    (currentSnake[0] + width >= 100 && direction === width) ||
+    (currentSnake[0] % width === 9 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+
+  )
+    return clearInterval(timerId)
+
+
+
+
   const tail = currentSnake.pop()
   squares[tail].classList.remove("snake")
   currentSnake.unshift(currentSnake[0] + direction)
